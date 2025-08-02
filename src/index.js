0c7dfbe5203bc516e001205558cc32e6a71ea053
ShubHH03 <mak.shubham2003@gmail.com>
2025-08-02 17:52:39 +0530

    implement custom ApiError and ApiResponse classes; add asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed: ", err);
+  });
 
 
 
@@ -23,6 +35,9 @@ connectDB()
 
 
 
+
+
+  
 /*
 import express from "express";
 const app = express()
