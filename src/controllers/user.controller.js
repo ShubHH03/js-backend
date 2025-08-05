b2378a06c7d2487bbcc7aff218ce9eb2a92d7335
ShubHH03 <mak.shubham2003@gmail.com>
2025-08-05 12:24:37 +0530

    implement user registration logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,36 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
-const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+    
+  // get user details from frontend
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+  console.log("username: ", username);
 
-export {registerUser}
+  // validation - not empty
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // check if user already exists: username, email
+  const userExists = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (userExists) {
+    throw new ApiError(409, "User with email or username already exits");
+  }
+
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+});
+
+export { registerUser };
